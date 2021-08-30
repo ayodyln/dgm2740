773283f275e98d32ae9e5390eac540b6d9e0558b
Dylan Smith <78442693+ayodyln@users.noreply.github.com>
2021-08-29 20:17:49 -0600

    finished basic card layout

--- js/app.js
+++ js/app.js
@@ -1,1 +1,43 @@
-import { assignments } from "./assingments";
+import { assignments } from "./assingments.js";
+
+const main = document.querySelector(".content");
+// const card = document.querySelector(".card");
+// const figure = document.querySelector(".fig");
+// const img = document.querySelector(".content-img");
+// const figcap = document.querySelector(".figcap");
+// const assignmentName = document.querySelector(".assignment-name");
+// const btn = document.querySelector(".btn");
+
+//creating function to display to DOM
+function displayWork(assignment) {
+  console.log(assignment);
+
+  assignment.forEach((el, i, arr) => {
+    let card = document.createElement("div");
+    card.className = "card";
+    let figure = document.createElement("figure");
+    figure.className = "fig";
+    let img = document.createElement("img");
+    img.className = "content-img";
+    let figcap = document.createElement("figure");
+    figcap.className = "figcap";
+    let assignmentName = document.createElement("h2");
+    assignmentName.className = "assignment-name";
+    let a = document.createElement("a");
+    a.className = "btn";
+    ///////////////////////////////////////////////////
+
+    img.src = el.img;
+    figcap.textContent = el.name;
+    a.textContent = `View`;
+    a.href = el.url;
+
+    figure.appendChild(img);
+    figure.appendChild(figcap);
+    figure.appendChild(a);
+    card.appendChild(figure);
+    main.appendChild(card);
+  });
+}
+
+displayWork(assignments);
--- js/assingments.js
+++ js/assingments.js
@@ -1,14 +1,16 @@
 export const assignments = [
     {
+        id: 1,
         name: 'Dude Ranch',
-        url: 'unit-1/index.html',
-        img: 'main-img/page.png',
+        url: `unit-1/index.html`,
+        img: 'img/1.png',
         discription: 'This Assignment Was Good.'
     },
     {
+        id: 2,
         name: 'Test Name',
-        url: 'dylnsworks.com',
-        img: 'main-img/page.png',
+        url: `dylnsworks.com`,
+        img: 'img/2.png',
         discription: 'this is a test'
     }
 ]
