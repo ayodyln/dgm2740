97f0c4c53341698bad51edcdd17a37668ea07a8f
Dylan Smith <78442693+ayodyln@users.noreply.github.com>
2021-08-29 19:33:17 -0600

    setting up main page

---
+++ js/assingments.js
@@ -0,0 +1,14 @@
+export const assignments = [
+    {
+        name: 'Dude Ranch',
+        url: 'unit-1/index.html',
+        img: 'main-img/page.png',
+        discription: 'This Assignment Was Good.'
+    },
+    {
+        name: 'Test Name',
+        url: 'dylnsworks.com',
+        img: 'main-img/page.png',
+        discription: 'this is a test'
+    }
+]
