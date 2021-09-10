42b1ee1df74772aeeee91e157b4927d51bbe2aae
Dylan Smith <78442693+ayodyln@users.noreply.github.com>
2021-09-10 14:43:15 -0600

    set up my js approach to displaying pictures.

---
+++ unit-3/js/gallery.js
@@ -0,0 +1,51 @@
+export const gallery = [
+    {
+        id: 1,
+        name: 'Pomegranate',
+        imgURL: 'img/gallery/gallery01.jpg',
+        imgSRC: 'https://pixabay.com/photos/pomegranate-fruits-food-sliced-3383814/'
+    },
+    {
+        id: 2,
+        name: 'Chestnut',
+        imgURL: 'img/gallery/gallery02.jpg',
+        imgSRC: 'https://pixabay.com/photos/chestnut-autumn-spur-open-1698730/'
+    },
+    {
+        id: 3,
+        name: 'Berries',
+        imgURL: 'img/gallery/gallery03.jpg',
+        imgSRC: 'https://pixabay.com/photos/berries-fruit-nutrition-blueberries-3504149/'
+    },
+    {
+        id: 4,
+        name: 'Apple',
+        imgURL: 'img/gallery/gallery04.jpg',
+        imgSRC: 'https://pixabay.com/photos/apple-water-droplets-fruit-moist-1122537/'
+    },
+    {
+        id: 5,
+        name: 'Fresh Cut Fruit',
+        imgURL: 'img/gallery/gallery05.jpg',
+        imgSRC: 'https://pixabay.com/photos/fresh-fruits-bowls-fruit-bowls-2305192/'
+    },
+    {
+        id: 6,
+        name: 'Macrons',
+        imgURL: 'img/gallery/gallery06.jpg',
+        imgSRC: 'https://pixabay.com/photos/macarons-raspberries-pastries-2548818/'
+    },
+    {
+        id: 7,
+        name: 'Pineapple',
+        imgURL: 'img/gallery/gallery07.jpg',
+        imgSRC: 'https://pixabay.com/photos/pineapple-slices-fruit-vitamins-636562/'
+    },
+    {
+        id: 8,
+        name: 'Strawberries',
+        imgURL: 'img/gallery/gallery08.jpg',
+        imgSRC: 'https://pixabay.com/photos/strawberry-fruit-bowl-food-berry-1330459/'
+    },
+    
+]
