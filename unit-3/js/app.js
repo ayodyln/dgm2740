de314bb76b744a755fee2b4b6b172e287ce5b1b4
Dylan Smith <78442693+ayodyln@users.noreply.github.com>
2021-09-10 17:46:28 -0600

    testing on devices

---
+++ unit-3/js/app.js
@@ -0,0 +1,31 @@
+import { gallery } from "./gallery.js";
+
+const content = document.querySelector(".content");
+
+function displayContent(gallery) {
+  console.log(gallery);
+
+  gallery.forEach((el, i, arr) => {
+    let figure = document.createElement("figure");
+    figure.className = "fig";
+    let img = document.createElement("img");
+    img.className = "fruit-img";
+    let figcap = document.createElement("figcaption");
+
+    /////////////////////////////////////////////////
+    // width="1600" height="500"
+    img.setAttribute("width", "1600");
+    img.setAttribute("height", "500");
+    ////////////////////////////////////////////////
+
+    img.src = el.imgURL;
+    figcap.textContent = el.name;
+
+    figure.appendChild(img);
+    figure.appendChild(figcap);
+
+    content.appendChild(figure);
+  });
+}
+
+displayContent(gallery);
