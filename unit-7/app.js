41796b5017e3abac964aea537368a3570cff0939
Dylan Smith <78442693+ayodyln@users.noreply.github.com>
2021-10-12 14:35:41 -0600

    setting up structure 2.0

---
+++ unit-7/app.js
@@ -0,0 +1,37 @@
+const lawyersInfo = [
+  {
+    name: `Jason Ellis`,
+    imgURL: `img/img01.jpg`,
+  },
+  {
+    name: `Alicia Florrick`,
+    imgURL: `img/img02.jpg`,
+  },
+  {
+    name: `Allis Jacobson`,
+    imgURL: `img/img03.jpg`,
+  },
+  {
+    name: `Allen Smith`,
+    imgURL: `img/img04.jpg`,
+  },
+];
+
+const blogInfo = [
+  {
+    title: `Blog Post #1`,
+    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo libero quos recusandae natus ab ipsum, ipsa est. Repellendus amet vitae numquam voluptates beatae voluptatum, aspernatur labore illo inventore rem eaque?`,
+  },
+  {
+    title: `Blog Post #2`,
+    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo libero quos recusandae natus ab ipsum, ipsa est. Repellendus amet vitae numquam voluptates beatae voluptatum, aspernatur labore illo inventore rem eaque?`,
+  },
+  {
+    title: `Blog Post #3`,
+    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo libero quos recusandae natus ab ipsum, ipsa est. Repellendus amet vitae numquam voluptates beatae voluptatum, aspernatur labore illo inventore rem eaque?`,
+  },
+  {
+    title: `Blog Post #4`,
+    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo libero quos recusandae natus ab ipsum, ipsa est. Repellendus amet vitae numquam voluptates beatae voluptatum, aspernatur labore illo inventore rem eaque?`,
+  },
+];
