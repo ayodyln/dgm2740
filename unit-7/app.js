76fcf49fd9dbe2b74a336e0d2f2802afad556f6d
Dylan Smith <78442693+ayodyln@users.noreply.github.com>
2021-10-12 18:10:16 -0600

    i have no clue what he wants me todo...

--- unit-7/app.js
+++ unit-7/app.js
@@ -35,3 +35,45 @@ const blogInfo = [
     content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo libero quos recusandae natus ab ipsum, ipsa est. Repellendus amet vitae numquam voluptates beatae voluptatum, aspernatur labore illo inventore rem eaque?`,
   },
 ];
+
+//Setting variables
+const lawyerDiv = document.querySelector(".lawyers"); //laywers div
+const ctaDiv = document.querySelector('.CTA');
+//Initializing function
+
+// LawyersDisplay function
+(function lawyerDisplay() {
+
+  lawyersInfo.forEach((el, i, arr) => {
+    console.log(el);
+    //creating elements
+    const lawyerCard = document.createElement("div");
+    const lawyerName = document.createElement("p");
+    const lawyerImg = document.createElement("img");
+    const lawyerBtn = document.createElement("button");
+
+    // giving element class names
+    lawyerCard.className = "column small-6 medium-3 large-2 lawyer-card";
+    lawyerName.className = "lawyer-name";
+    lawyerImg.className = "lawyer-img";
+
+    //
+
+    ///Setting Attributes///
+    lawyerImg.setAttribute("width", "1600");
+    lawyerImg.setAttribute("height", "500");
+    ///////////////////////
+
+    lawyerName.textContent = el.name;
+    lawyerImg.src = el.imgURL;
+
+    lawyerBtn.textContent = `Read More`;
+
+    lawyerCard.appendChild(lawyerName);
+    lawyerCard.appendChild(lawyerImg);
+    lawyerCard.appendChild(lawyerBtn);
+    lawyerDiv.appendChild(lawyerCard);
+  });
+})();
+
+
