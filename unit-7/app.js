0b33f20b4e34244ad0ce99461570fac77e24cbbd
Dylan Smith <78442693+ayodyln@users.noreply.github.com>
2021-10-13 13:16:59 -0600

    blog section mobile done

--- unit-7/app.js
+++ unit-7/app.js
@@ -20,18 +20,22 @@ const lawyersInfo = [
 const blogInfo = [
   {
     title: `Blog Post #1`,
+    imgURL: `img/blog01.jpg`,
     content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo libero quos recusandae natus ab ipsum, ipsa est. Repellendus amet vitae numquam voluptates beatae voluptatum, aspernatur labore illo inventore rem eaque?`,
   },
   {
     title: `Blog Post #2`,
+    imgURL: `img/blog02.jpg`,
     content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo libero quos recusandae natus ab ipsum, ipsa est. Repellendus amet vitae numquam voluptates beatae voluptatum, aspernatur labore illo inventore rem eaque?`,
   },
   {
     title: `Blog Post #3`,
+    imgURL: `img/blog03.jpg`,
     content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo libero quos recusandae natus ab ipsum, ipsa est. Repellendus amet vitae numquam voluptates beatae voluptatum, aspernatur labore illo inventore rem eaque?`,
   },
   {
     title: `Blog Post #4`,
+    imgURL: `img/blog04.jpg`,
     content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo libero quos recusandae natus ab ipsum, ipsa est. Repellendus amet vitae numquam voluptates beatae voluptatum, aspernatur labore illo inventore rem eaque?`,
   },
 ];
@@ -39,6 +43,7 @@ const blogInfo = [
 //Setting variables
 const lawyerDiv = document.querySelector(".lawyers"); //laywers div
 const ctaDiv = document.querySelector(".CTA");
+const blogPost = document.querySelector(".blog-post");
 //Initializing function
 
 // LawyersDisplay function
@@ -75,10 +80,37 @@ const ctaDiv = document.querySelector(".CTA");
   });
 })();
 
-
 //blog posts
 (function blogPostsDisplay() {
-  blogInfo.forEach((el, i , arr) => {
-    console.log(el)
-  })
+  blogInfo.forEach((el, i, arr) => {
+    console.log(el);
+
+    const blogPostCard = document.createElement("div");
+    const blogImg = document.createElement("img");
+    const blogTitle = document.createElement("h2");
+    const blogContent = document.createElement("p");
+    const readMoreBtn = document.createElement("button");
+
+    //giving class names
+    blogPostCard.className = "column small-12 medium-6 large-3 blog-cards";
+    blogImg.className = "blog-img";
+    blogTitle.className = "blog-title";
+    blogContent.className = "blog-content";
+    readMoreBtn.className = "read-more-btn";
+    ///Setting Attributes///
+    blogImg.setAttribute("width", "1600");
+    blogImg.setAttribute("height", "500");
+    ///////////////////////
+
+    blogImg.src = el.imgURL;
+    blogTitle.textContent = el.title;
+    blogContent.textContent = el.content;
+    readMoreBtn.textContent = `Read More`;
+
+    blogPostCard.appendChild(blogImg);
+    blogPostCard.appendChild(blogTitle);
+    blogPostCard.appendChild(blogContent);
+    blogPostCard.appendChild(readMoreBtn);
+    blogPost.appendChild(blogPostCard);
+  });
 })();
