d5d12ad9d5489fad98a86fa48f658358ed7b44db
Dylan Smith <78442693+ayodyln@users.noreply.github.com>
2021-10-15 18:57:42 -0600

    assignment page update draft1.0

--- js/app.js
+++ js/app.js
@@ -8,6 +8,63 @@ const main = document.querySelector(".content");
 // const assignmentName = document.querySelector(".assignment-name");
 // const btn = document.querySelector(".btn");
 
+// updated displayWorkFunction
+function mobileDisplayClassWork(assignment) {
+  assignment.forEach((el, i, arr) => {
+    let classCard = document.createElement("div");
+    let cardPrimary = document.createElement("div");
+    let assignmentFig = document.createElement('fig')
+    let assignmentImg = document.createElement('img')
+    let assignmentTitle = document.createElement("h2");
+    let assignmentInfo = document.createElement("p");
+    let learnBtn = document.createElement("button");
+    let visitBtn = document.createElement("a");
+
+    learnBtn.addEventListener("click", () => {
+      assignmentInfo.classList.toggle("hidden");
+      learnBtn.classList.toggle("btn-open");
+
+      if (learnBtn.classList.contains("btn-open")) {
+        learnBtn.style.borderRadius = "0px 0px 0px 0px";
+      } else {
+        learnBtn.style.borderRadius = "0px 0px 10px 10px";
+      }
+    });
+
+    ////////////////////////////////////////////////////
+    classCard.className = `class-card`;
+    cardPrimary.className = "upper-layer";
+    assignmentTitle.className = `assignment-title`;
+    assignmentInfo.className = `assignment-info hidden`;
+    assignmentImg.className = 'card-img'
+    learnBtn.className = `learn-btn`;
+    visitBtn.className = `visit-btn`;
+
+    ////////////////////////////////////////////////////
+
+    ////////////////////////////////////////////////////
+
+    ////////////////////////////////////////////////////
+
+    assignmentTitle.textContent = `Unit-${el.id} ${el.name}`;
+    assignmentImg.src = el.img
+    assignmentInfo.textContent = el.discription;
+    visitBtn.textContent = `Visit Site`;
+    visitBtn.href = el.url;
+    learnBtn.className = "fas fa-caret-down";
+
+    
+    cardPrimary.appendChild(assignmentImg)
+    cardPrimary.appendChild(assignmentTitle);
+    cardPrimary.appendChild(visitBtn);
+    classCard.appendChild(cardPrimary);
+    classCard.appendChild(learnBtn);
+    classCard.appendChild(assignmentInfo);
+    main.appendChild(classCard);
+  });
+}
+mobileDisplayClassWork(assignments);
+
 //creating function to display to DOM
 function displayWork(assignment) {
   console.log(assignment);
@@ -25,11 +82,11 @@ function displayWork(assignment) {
     assignmentName.className = "assignment-name";
     let a = document.createElement("a");
     a.className = "btn";
-    let btnDiv = document.createElement('div')
-    btnDiv.className = 'btn-div'
+    let btnDiv = document.createElement("div");
+    btnDiv.className = "btn-div";
     ///////////////////////////////////////////////////
-    img.setAttribute("width", "1600")
-    img.setAttribute("height", "500")
+    img.setAttribute("width", "1600");
+    img.setAttribute("height", "500");
     ///////////////////////////////////////////////////
 
     img.src = el.img;
@@ -40,37 +97,36 @@ function displayWork(assignment) {
     figure.appendChild(img);
     figure.appendChild(figcap);
     figure.appendChild(btnDiv);
-    btnDiv.appendChild(a)
+    btnDiv.appendChild(a);
     card.appendChild(figure);
-    
+
     main.appendChild(card);
   });
 }
 
-displayWork(assignments);
-
+// displayWork(assignments);
 
 //displaying surfaces for small device
 
 function smallDevice(assignment) {
   assignment.forEach((el, i, arr) => {
-    let surface = document.createElement('div')
-    surface.className = 'surface'
-    let h3  = document.createElement('h3')
-    let a = document.createElement('a')
+    let surface = document.createElement("div");
+    surface.className = "surface";
+    let h3 = document.createElement("h3");
+    let a = document.createElement("a");
 
     ///////////////////////////////////////////////
     //////////////////////////////////////////////
 
     h3.textContent = el.name;
-    a.textContent = 'View'
+    a.textContent = "View";
     a.href = el.url;
 
-    surface.appendChild(h3)
-    surface.appendChild(a)
+    surface.appendChild(h3);
+    surface.appendChild(a);
 
-    main.appendChild(surface)
-  })
+    main.appendChild(surface);
+  });
 }
 
-smallDevice(assignments)
+// smallDevice(assignments)
--- js/assingments.js
+++ js/assingments.js
@@ -1,58 +1,58 @@
 export const assignments = [
-    {
-        id: 1,
-        name: 'Dude Ranch',
-        url: `unit-1/index.html`,
-        img: 'img/1.png',
-        discription: 'Dude Ranch Assignment'
-    },
-    {
-        id: 2,
-        name: 'Science Museum',
-        url: `unit-2/index.html`,
-        img: 'unit-2/img/science-muesum-logo-quick.png',
-        discription: 'Science Muesum Assignment'
-    },
-    {
-        id: 3,
-        name: 'Fruit Stand',
-        url: `unit-3/index.html`,
-        img: 'unit-3/img/fruit-stand-logo.png',
-        discription: 'Fruit Stand Assignment'
-    },
-    {
-        id: 4,
-        name: 'National Forest',
-        url: `unit-4/index.html`,
-        img: `unit-4/img/bison.png`,
-        discription: 'National Forest Assignment '
-    },
-    {
-        id: 5,
-        name: 'Restaurant',
-        url: `unit-5/index.html`,
-        img: `unit-5/img/sushi-logo-original.png`,
-        discription: 'Restaruant Responsive Image Assignment'
-    },
-    {
-        id: 6,
-        name: 'Band',
-        url: `unit-6/index.html`,
-        img: `unit-6/img/guitar-assignment-page.jpg`,
-        discription: 'Band Lazy Loading Image Assignment'
-    },
-    {
-        id: 7,
-        name: 'Law Firm',
-        url: 'unit-7/index.html',
-        img: `unit-7/img/blog04.jpg`,
-        discription: 'Created a custom frame work with SCSS, the site has a responsive 12 grid system.'
-    },
-    {
-        id: 8,
-        name: 'News Site',
-        url: 'unit-8/index.html',
-        img: `unit-8/img/newspaper.png`,
-        discription: `This site is a test site for building a grid using CSS. I'm now able to build a site quickly wiht just CSS Grid.` 
-    }
-]
+  {
+    id: 1,
+    name: "Dude Ranch",
+    url: `unit-1/index.html`,
+    img: "img/1.png",
+    discription: `Dude Ranch Assignment was a semester-start assignment. Essentially was a review assignment from what was learned in early 2021.`,
+  },
+  {
+    id: 2,
+    name: "Science Museum",
+    url: `unit-2/index.html`,
+    img: "unit-2/img/science-muesum-logo-quick.png",
+    discription: `Science Muesum Assignment was an introduction to SCSS and I further explored SASS on my own time. The outcome of this assignment was knowledge of how to build a site using SCSS.`,
+  },
+  {
+    id: 3,
+    name: "Fruit Stand",
+    url: `unit-3/index.html`,
+    img: "unit-3/img/fruit-stand-logo.png",
+    discription: `Fruit Stand Assignment was an assignment to practice with working images and proccessing them with Photoshop, and intoduction to lighten and darken SCSS functions.`,
+  },
+  {
+    id: 4,
+    name: "National Forest",
+    url: `unit-4/index.html`,
+    img: `unit-4/img/bison.png`,
+    discription: `National Forest Assignment was practice to building responsive images and introduction to CSS animations.`,
+  },
+  {
+    id: 5,
+    name: "Restaurant",
+    url: `unit-5/index.html`,
+    img: `unit-5/img/sushi-logo-original.png`,
+    discription: `Restaruant Responsive Image Assignment is a continuation of builing responsive images for websites. This assignment though implementing pixel density images that are displayed to specific devices. Outcome of this assignment was knowledge of how to provide a great image viewing experince for users.`,
+  },
+  {
+    id: 6,
+    name: "Band",
+    url: `unit-6/index.html`,
+    img: `unit-6/img/guitar-assignment-page.jpg`,
+    discription: `Band Lazy Loading Image Assignment was building upon the last assignment, Restaurant Unit- 5. However, I've learned the basics of how to implement Lazy image loading effeciently and took the assignment further to practice with some Javascript as well.`,
+  },
+  {
+    id: 7,
+    name: "Law Firm",
+    url: "unit-7/index.html",
+    img: `unit-7/img/blog04.jpg`,
+    discription: `Created a custom frame work with SCSS, the site has a responsive 12 grid system.`,
+  },
+  {
+    id: 8,
+    name: "News Site",
+    url: "unit-8/index.html",
+    img: `unit-8/img/newspaper.png`,
+    discription: `This site is a test site for building a grid using CSS. I'm now able to build a site quickly wiht just CSS Grid.`,
+  },
+];
