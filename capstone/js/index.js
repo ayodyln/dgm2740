a58d069196c22b071ba1dbd12efaa4cc704bd8a2
Dylan Smith <78442693+ayodyln@users.noreply.github.com>
2021-11-29 13:33:22 -0700

    test

---
+++ capstone/js/index.js
@@ -0,0 +1,19 @@
+// grabbing DOM elements
+const navBtn = document.getElementById("navBtn");
+
+navBtn.addEventListener("click", () => {
+  console.log("✔️");
+
+  //? Grabbing main nav elements to properly toggle them off and on for presentation
+
+  document.querySelector(".navBtnDiv").classList.toggle("Open");
+  document.querySelector(".open").classList.toggle("hidden");
+  document.querySelector(".close").classList.toggle("hidden");
+  document.querySelector(".mainUL").classList.toggle("hidden");
+});
+
+const subNav = document.querySelector(".subNavLi");
+
+subNav.addEventListener("mouseover", () => {
+  document.querySelector(".subNav").classList.toggle("hidden");
+});
